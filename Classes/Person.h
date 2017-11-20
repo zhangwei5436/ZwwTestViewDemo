bd5c30aec255672233cb84f2e6206844f3a6273d
张威 <825460068@qq.com>
2017-11-20 17:45:59 +0800

    提交

---
+++ Classes/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  SliderDemo
+//
+//  Created by 张威 on 2017/11/20.
+//  Copyright © 2017年 张威. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  SliderDemo
+//
+//  Created by 张威 on 2017/11/20.
+//  Copyright © 2017年 张威. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
